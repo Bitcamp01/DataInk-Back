db8a23655bbb05f464eb0a12246ed055a825fc29
Rurouni98 <byungju98@gmail.com>
2024-10-07 22:28:44 +0900

    Modify: 1007 김병주 커밋 되돌아간 거 일부 복구

--- dataink-back/src/main/java/com/bit/datainkback/dto/LabelTaskDto.java
+++ dataink-back/src/main/java/com/bit/datainkback/dto/LabelTaskDto.java
@@ -1,4 +1,31 @@
 package com.bit.datainkback.dto;
 
+import com.bit.datainkback.enums.TaskLevel;
+import com.bit.datainkback.enums.TaskStatus;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
 public class LabelTaskDto {
+
+    private Long taskId;
+    private Long projectId;
+    private Long userId;
+    private TaskStatus status;
+    private TaskLevel level;
+    private String comment;
+    private String rejectionReason;
+    private LocalDateTime create;
+    private LocalDateTime update;
+    private LocalDateTime submit;
+    private LocalDateTime review;
+    private LocalDateTime approve;
+    private Long sourceDataId;
+    private Long labelFieldId;
 }
---
+++ dataink-back/src/main/java/com/bit/datainkback/enums/TaskLevel.java
@@ -0,0 +1,7 @@
+package com.bit.datainkback.enums;
+
+public enum TaskLevel {
+    LABELER,    // 라벨러 레벨
+    REVIEWER,   // 검수자 레벨
+    ADMIN       // 관리자 레벨
+}
