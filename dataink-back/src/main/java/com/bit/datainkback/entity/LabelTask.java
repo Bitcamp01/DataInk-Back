d9c216ef6e621367918197f6b46ed60762a976a5
bit\bitcamp <byungju98@gmail.com>
2024-10-07 20:34:25 +0900

    Add: 1007 김병주 LabelTask dto 및 entity 작성, SourceDataDto 추가
    1. LabelTask dto 및 entity 작성, 아직 미작성한 entity랑 연결하는 부분은 주석처리
    2. SourceDataDto 필요해보여서 추가
    3. enum 폴더 및 파일 몇개 작성

--- dataink-back/src/main/java/com/bit/datainkback/entity/LabelTask.java
+++ dataink-back/src/main/java/com/bit/datainkback/entity/LabelTask.java
@@ -1,4 +1,82 @@
 package com.bit.datainkback.entity;
 
+import com.bit.datainkback.dto.LabelTaskDto;
+import com.bit.datainkback.enums.TaskLevel;
+import com.bit.datainkback.enums.TaskStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "LABEL_TASK")
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class LabelTask {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "task_id")
+    private Long taskId;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "project_id")
+//    private Project project;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id")
+//    private User user;
+
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+
+    @Enumerated(EnumType.STRING)
+    private TaskLevel level;
+
+    private String comment;
+
+    @Column(name = "rejection_reason")
+    private String rejectReason;
+
+    private LocalDateTime create;
+
+    private LocalDateTime update;
+
+    private LocalDateTime submit;
+
+    private LocalDateTime review;
+
+    private LocalDateTime approve;
+
+//    @OneToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "source_id")
+//    private SourceData sourceData;
+//
+//    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+//    @JoinColumn(name = "field_id")
+//    private LabelField labelField;
+
+    public LabelTaskDto toDto() {
+        return LabelTaskDto.builder()
+                .taskId(this.taskId)
+//                .projectId(this.project.getProjectId())
+//                .userId(this.user.getUserId())
+                .status(this.status)
+                .level(this.level)
+                .comment(this.comment)
+                .rejectionReason(this.rejectReason)
+                .create(this.create)
+                .update(this.update)
+                .submit(this.submit)
+                .review(this.review)
+                .approve(this.approve)
+//                .sourceDataId(this.sourceData.getSourceId())
+//                .labelFieldId(this.labelField.getFieldId())
+                .build();
+    }
+
 }
