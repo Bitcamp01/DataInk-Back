968d4c82daaba74b214b3794965fd4235b8208d8
bitcamp\BIT <0215bitcamp@gmail.com>
2024-10-08 09:48:47 +0900

    ADD 프로젝트 구조 엔티티, dto추가

--- dataink-back/src/main/java/com/bit/datainkback/dto/ProjectDto.java
+++ dataink-back/src/main/java/com/bit/datainkback/dto/ProjectDto.java
@@ -1,4 +1,28 @@
 package com.bit.datainkback.dto;
+import com.bit.datainkback.entity.Project;
+import com.bit.datainkback.entity.User;
+import lombok.*;
 
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ProjectDto {
+    private int projectId;
+    private int userId;
+    private String name;
+    private String description;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private String mongoDataId;
+
+    public Project toEntity(User user) {
+        return Project.builder().projectId(this.projectId).owner(user)
+                .startDate(this.startDate).endDate(this.endDate).description(this.description)
+                .mongoDataId(this.mongoDataId).build();
+    }
 }
+
--- dataink-back/src/main/java/com/bit/datainkback/entity/Project.java
+++ dataink-back/src/main/java/com/bit/datainkback/entity/Project.java
@@ -1,4 +1,47 @@
 package com.bit.datainkback.entity;
 
+import com.bit.datainkback.dto.ProjectDto;
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "PROJECT")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "project_id")
+    private int projectId;
+
+    @ManyToOne
+    @JoinColumn(name = "owner", nullable = false)
+    private User owner;
+
+    @Column(name = "name", nullable = false, length = 255)
+    private String name;
+
+    @Column(name = "desc", length = 255)
+    private String description;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDateTime startDate;
+
+    @Column(name = "end_date", nullable = false)
+    private LocalDateTime endDate;
+
+    @Column(name = "mongo_data_id", length = 255)
+    private String mongoDataId;
+    public ProjectDto toDto() {
+        return ProjectDto.builder().projectId(this.projectId).userId(this.owner.getUserId())
+                .startDate(this.startDate).endDate(this.endDate).description(this.description)
+                .mongoDataId(this.mongoDataId).build();
+    }
 }
