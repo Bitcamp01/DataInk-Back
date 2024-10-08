d167047239df22a4352a4ae408e6e0f84d6d9acb
bit\bitcamp <1>
2024-10-08 20:50:32 +0900

    241008 김시표 ADD:로그인 회원가입 로그아웃 컨트롤러 작성중

--- dataink-back/src/main/java/com/bit/datainkback/service/UserService.java
+++ dataink-back/src/main/java/com/bit/datainkback/service/UserService.java
@@ -1,10 +1,13 @@
 package com.bit.datainkback.service;
 
-import org.springframework.stereotype.Service;
-
+import com.bit.datainkback.dto.UserDto;
 import java.util.Map;
 
 public interface UserService {
+    Map<String, String> idCheck(String userId);
 
-    Map<String, String> idCheck(String id);
+    UserDto join(UserDto userDto);
+
+    UserDto login(UserDto userDto);
+    void logout();
 }
