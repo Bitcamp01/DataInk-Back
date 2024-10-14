10fe8193b221ef9c51db0f3180e4291b776642eb
bit\bitcamp <byungju98@gmail.com>
2024-10-14 12:24:07 +0900

    Add: 1014 김병주 라벨링 데이터 구조 변경에 따른 service, repo, controller 수정

---
+++ dataink-back/src/main/java/com/bit/datainkback/controller/mongo/FolderController.java
@@ -0,0 +1,45 @@
+package com.bit.datainkback.controller.mongo;
+
+import com.bit.datainkback.entity.mongo.Folder;
+import com.bit.datainkback.service.mongo.FolderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/mongo/folders")
+public class FolderController {
+    @Autowired
+    private FolderService folderService;
+
+    // 폴더 생성
+    @PostMapping
+    public Folder createFolder(@RequestBody Folder folder) {
+        return folderService.createFolder(folder);
+    }
+
+    // 특정 폴더 조회
+    @GetMapping("/{id}")
+    public Folder getFolderById(@PathVariable String id) {
+        return folderService.getFolderById(id);
+    }
+
+    // 모든 폴더 조회
+    @GetMapping
+    public List<Folder> getAllFolders() {
+        return folderService.getAllFolders();
+    }
+
+    // 폴더 삭제
+    @DeleteMapping("/{id}")
+    public void deleteFolder(@PathVariable String id) {
+        folderService.deleteFolder(id);
+    }
+
+    // 폴더의 fields 업데이트
+//    @PutMapping("/{id}/fields")
+//    public Folder updateFolderFields(@PathVariable String id, @RequestBody List<Field> fields) {
+//        return folderService.updateFolderFields(id, fields);
+//    }
+}
---
+++ dataink-back/src/main/java/com/bit/datainkback/service/mongo/FolderService.java
@@ -0,0 +1,43 @@
+package com.bit.datainkback.service.mongo;
+
+import com.bit.datainkback.entity.mongo.Folder;
+import com.bit.datainkback.repository.mongo.FolderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class FolderService {
+
+    @Autowired
+    private FolderRepository folderRepository;
+
+    // 폴더 생성
+    public Folder createFolder(Folder folder) {
+        return folderRepository.save(folder);
+    }
+
+    // 특정 폴더 조회
+    public Folder getFolderById(String id) {
+        return folderRepository.findById(id).orElse(null);
+    }
+
+    // 모든 폴더 조회
+    public List<Folder> getAllFolders() {
+        return folderRepository.findAll();
+    }
+
+    // 폴더 삭제
+    public void deleteFolder(String id) {
+        folderRepository.deleteById(id);
+    }
+
+    // 폴더의 fields 업데이트
+//    public Folder updateFolderFields(String folderId, List<Field> fields) {
+//        Folder folder = folderRepository.findById(folderId)
+//                .orElseThrow(() -> new RuntimeException("폴더를 찾을 수 없습니다."));
+//        folder.setFields(fields);
+//        return folderRepository.save(folder);
+//    }
+}
+
