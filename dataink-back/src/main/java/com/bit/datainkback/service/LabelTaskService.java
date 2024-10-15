457923b7149163709966fda1f7c17306a6ff7164
Taelim Ma <tlma001003@gmail.com>
2024-10-15 18:52:37 +0900

    Modify : 1015 마태림 승인 반려 컨트롤러 서비스 서비스임플 구현 / 프론트 연결과 데이터 형식 변환 로직은 아직 활성화 시키지 않음

--- dataink-back/src/main/java/com/bit/datainkback/service/LabelTaskService.java
+++ dataink-back/src/main/java/com/bit/datainkback/service/LabelTaskService.java
@@ -3,15 +3,20 @@ package com.bit.datainkback.service;
 import com.bit.datainkback.dto.RejectReasonDto;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @Service
 public interface LabelTaskService {
-    <LabelTaskDTO> List<LabelTaskDTO> getAllTasks();
+//    <LabelTaskDTO> List<LabelTaskDTO> getAllTasks();
+//
+//    <LabelTaskDTO> LabelTaskDTO getTaskById(Long taskId);
 
-    <LabelTaskDTO> LabelTaskDTO getTaskById(Long taskId);
+//    void approveTask(Long taskId);
 
-    void approveTask(Long taskId);
+    // 반려 시
+    void rejectLabelTask(Long taskId, String rejectionReason, Timestamp reviewedTimestamp);
+    // 승인 시
+    void approveLabelTask(Long taskId, String comment, Timestamp reviewedTimestamp);
 
-    void rejectTask(Long taskId, RejectReasonDto reasonDto);
 }
