d8fade1782eecfea3f4d0ec7f8e201d30cb20ff6
WIN-HR35D92QS4P\soominchoi <csm0062@naver.com>
2024-10-16 21:05:45 +0900

    Modify:레포지토리 어노테이션 붙여줌

--- dataink-back/src/main/java/com/bit/datainkback/repository/UserDetailRepository.java
+++ dataink-back/src/main/java/com/bit/datainkback/repository/UserDetailRepository.java
@@ -1,4 +1,9 @@
 package com.bit.datainkback.repository;
 
-public interface UserDetailRepository {
+import com.bit.datainkback.entity.UserDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDetailRepository{
 }
