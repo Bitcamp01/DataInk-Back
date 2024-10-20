1c531e4605b51672aba64892883c9cfc8a0e38c5
LAPTOP-LRGD8QT0\byung <byungju98@gmail.com>
2024-10-20 12:12:59 +0900

    Modify: 1020 김병주 폴더 트리구조 반환을 위해 엔티티 및 서비스 수정

--- dataink-back/src/main/java/com/bit/datainkback/entity/mongo/Folder.java
+++ dataink-back/src/main/java/com/bit/datainkback/entity/mongo/Folder.java
@@ -1,11 +1,9 @@
 package com.bit.datainkback.entity.mongo;
 
+import com.bit.datainkback.dto.mongo.FolderDto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -16,6 +14,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Folder {
     @Id
     private String id;  // MongoDB의 ObjectId를 사용하여 ID 생성
@@ -31,5 +30,18 @@ public class Folder {
     // MongoDB에 삽입할 때 String으로 변환된 ObjectId를 id에 설정하는 메서드
     public void generateId() {
         this.id = new ObjectId().toString();  // 자동 생성된 ObjectId를 String으로 변환
+    }
+
+    public FolderDto toDto() {
+        return FolderDto.builder()
+                .id(this.id)
+                .label(this.label)
+                .itemIds(this.itemIds)
+                .lastModifiedUserId(this.lastModifiedUserId)
+                .lastModifiedDate(this.lastModifiedDate)
+                .isFolder(this.isFolder)
+                .finished(this.finished)
+                .children(this.children != null ? this.children.stream().map(Folder::toDto).toList() : null)
+                .build();
     }
 }
