f49837085cdce64ef443519f5645312de77f24a8
bit\bitcamp <havebeen4@naver.com>
2024-10-21 19:52:45 +0900

    Add: 1021 오유빈 WorkIn과 Calendar 백엔드 연결 완료

--- dataink-back/src/main/java/com/bit/datainkback/repository/ProjectRepository.java
+++ dataink-back/src/main/java/com/bit/datainkback/repository/ProjectRepository.java
@@ -1,4 +1,13 @@
 package com.bit.datainkback.repository;
 
-public class ProjectRepository {
+import com.bit.datainkback.entity.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+    List<Project> findByUser_UserId(Long userId);
 }
