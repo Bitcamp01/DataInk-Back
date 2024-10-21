e21121140e8cc7f7566b79b39ca9e693dc3bb15b
Taelim Ma <tlma001003@gmail.com>
2024-10-21 15:30:41 +0900

    Modify : 1021 마태림 전체 오류 나지 않게 수정. 근데 id 받아오는 부분이 미흡해서 반려 승인 정상 작동은 안됨

--- dataink-back/src/main/java/com/bit/datainkback/service/LabelTaskService.java
+++ dataink-back/src/main/java/com/bit/datainkback/service/LabelTaskService.java
@@ -1,5 +1,6 @@
 package com.bit.datainkback.service;
 
+import com.bit.datainkback.dto.LabelTaskDto;
 import com.bit.datainkback.dto.RejectReasonDto;
 import org.springframework.stereotype.Service;
 
@@ -8,15 +9,11 @@ import java.util.List;
 
 @Service
 public interface LabelTaskService {
-//    <LabelTaskDTO> List<LabelTaskDTO> getAllTasks();
-//
-//    <LabelTaskDTO> LabelTaskDTO getTaskById(Long taskId);
-
-//    void approveTask(Long taskId);
 
     // 반려 시
     void rejectLabelTask(Long taskId, String rejectionReason, Timestamp reviewedTimestamp);
     // 승인 시
     void approveLabelTask(Long taskId, String comment, Timestamp reviewedTimestamp);
 
+    List<LabelTaskDto> getAllLabelTasks();
 }
