36fe7a1af280b133c6a5e99b27f1d978f145fd49
bit\bitcamp <byungju98@gmail.com>
2024-10-23 20:58:22 +0900

    Modify: 1023 김병주 WebMvc 설정 url 변경

--- dataink-back/src/main/java/com/bit/datainkback/config/WebMvcConfiguration.java
+++ dataink-back/src/main/java/com/bit/datainkback/config/WebMvcConfiguration.java
@@ -9,7 +9,7 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:3001")
+                .allowedOrigins("http://223.130.134.24:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true)
