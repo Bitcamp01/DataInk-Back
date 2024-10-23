96b25496f6f6b6a79d3228c33c32135be580c018
bit\bitcamp <byungju98@gmail.com>
2024-10-23 19:44:14 +0900

    Modify: 1023 김병주 라벨링 페이지 검색 가능하게 수정중

--- dataink-back/src/main/java/com/bit/datainkback/dto/mongo/FolderDto.java
+++ dataink-back/src/main/java/com/bit/datainkback/dto/mongo/FolderDto.java
@@ -21,6 +21,10 @@ public class FolderDto {
     private boolean isFolder;
     private boolean finished;
     private List<FolderDto> children;  // Folder의 하위 폴더도 Dto로 변환
+    private String category1;
+    private String category2;
+    private String category3;
+    private String workStatus;
 
     // FolderDto -> Folder 엔티티로 변환
     public Folder toEntity() {
@@ -33,6 +37,10 @@ public class FolderDto {
                 .isFolder(this.isFolder)
                 .finished(this.finished)
                 .children(this.children != null ? this.children.stream().map(FolderDto::toEntity).toList() : null)
+                .category1(this.category1)
+                .category2(this.category2)
+                .category3(this.category3)
+                .workStatus(this.workStatus)
                 .build();
     }
 }
--- dataink-back/src/main/java/com/bit/datainkback/entity/mongo/Folder.java
+++ dataink-back/src/main/java/com/bit/datainkback/entity/mongo/Folder.java
@@ -3,6 +3,7 @@ package com.bit.datainkback.entity.mongo;
 import com.bit.datainkback.dto.mongo.FolderDto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
 import lombok.*;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -27,6 +28,15 @@ public class Folder {
     private boolean finished; // 하위 작업 완료 여부
     private List<Folder> children;  // 하위 폴더 및 파일(Task) 통합
 
+    @Transient
+    private String category1;
+    @Transient
+    private String category2;
+    @Transient
+    private String category3;
+    @Transient
+    private String workStatus;
+
     // MongoDB에 삽입할 때 String으로 변환된 ObjectId를 id에 설정하는 메서드
     public void generateId() {
         this.id = new ObjectId().toString();  // 자동 생성된 ObjectId를 String으로 변환
@@ -42,6 +52,10 @@ public class Folder {
                 .isFolder(this.isFolder)
                 .finished(this.finished)
                 .children(this.children != null ? this.children.stream().map(Folder::toDto).toList() : null)
+                .category1(this.category1)
+                .category2(this.category2)
+                .category3(this.category3)
+                .workStatus(this.workStatus)
                 .build();
     }
 }
