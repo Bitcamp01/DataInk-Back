7da8c645d01cd7406860bbf2b64f3c4358975fd6
WIN-HR35D92QS4P\soominchoi <csm0062@naver.com>
2024-10-29 20:49:08 +0900

    Modify: workStatus 검색바 끝 조건별로 다 찾을 수 있게 해놓음 프로젝트조건, 기간조건 검색조건 전부

--- dataink-back/src/main/java/com/bit/datainkback/repository/custom/ProjectRepositoryCustom.java
+++ dataink-back/src/main/java/com/bit/datainkback/repository/custom/ProjectRepositoryCustom.java
@@ -5,7 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public interface ProjectRepositoryCustom {
-    Page<Project> searchAll(String searchCondition, String searchKeyword, Pageable pageable, LocalDateTime startDate, LocalDateTime endDate, Long loggedInUserId);
+    Page<Project> searchAll(String searchCondition, String searchKeyword, Pageable pageable, LocalDateTime startDate, LocalDateTime endDate, List<Long> projectIds);
 }
