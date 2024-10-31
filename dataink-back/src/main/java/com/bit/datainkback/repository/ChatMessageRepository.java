9efc2434136178d93892d26f792adcc4b8e309b7
bitcamp\BIT <0215bitcamp@gmail.com>
2024-10-31 15:20:21 +0900

    no message

---
+++ dataink-back/src/main/java/com/bit/datainkback/repository/ChatMessageRepository.java
@@ -0,0 +1,11 @@
+package com.bit.datainkback.repository;
+
+import com.bit.datainkback.entity.ChatMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+    List<ChatMessage> findBySenderIdAndReceiverIdOrReceiverIdAndSenderId(
+            String senderId, String receiverId, String receiverId2, String senderId2);
+}
