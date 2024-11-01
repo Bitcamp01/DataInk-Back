b09857499240ab29bc1d732f7ed05d4e2b008724
bit\bitcamp <1>
2024-11-01 19:57:52 +0900

    MODIFY 김시표 민감정보땜에 복구중

--- dataink-back/src/main/java/com/bit/datainkback/config/RedisConfiguration.java
+++ dataink-back/src/main/java/com/bit/datainkback/config/RedisConfiguration.java
@@ -4,9 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -14,10 +12,7 @@ public class RedisConfiguration {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
-        config.setHostName("redisc-2vhvej.vpc-cdb.ntruss.com");
-        config.setPort(6379);
-        return new JedisConnectionFactory(config);
+        return new JedisConnectionFactory();
     }
 
     @Bean
@@ -27,7 +22,7 @@ public class RedisConfiguration {
 
         // String 키와 Object 값의 직렬화 설정
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
 
         return template;
     }
--- dataink-back/src/main/java/com/bit/datainkback/service/impl/NotificationServiceImpl.java
+++ dataink-back/src/main/java/com/bit/datainkback/service/impl/NotificationServiceImpl.java
@@ -1,12 +1,15 @@
 package com.bit.datainkback.service.impl;
 
+import com.bit.datainkback.dto.NotificationDto;
 import com.bit.datainkback.entity.Notification;
 import com.bit.datainkback.repository.NotificationRepository;
+import com.bit.datainkback.repository.UserRepository;
 import com.bit.datainkback.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class NotificationServiceImpl implements NotificationService {
@@ -17,14 +20,30 @@ public class NotificationServiceImpl implements NotificationService {
         this.notificationRepository = notificationRepository;
     }
 
+//    @Override
+//    // 특정 사용자의 알림 목록 조회 (미읽음 알림만 포함 가능)
+//    public List<Notification> getNotificationsByUserId(Long userId, boolean unreadOnly) {
+//        if (unreadOnly) {
+//            return notificationRepository.findByUser_UserIdAndIsReadFalse(userId);
+//        } else {
+//            return notificationRepository.findByUser_UserIdOrderByCreatedAtDesc(userId);
+//        }
+//    }
+
     @Override
-    // 특정 사용자의 알림 목록 조회 (미읽음 알림만 포함 가능)
-    public List<Notification> getNotificationsByUserId(Long userId, boolean unreadOnly) {
-        if (unreadOnly) {
-            return notificationRepository.findByUser_UserIdAndIsReadFalse(userId);
-        } else {
-            return notificationRepository.findByUser_UserIdOrderByCreatedAtDesc(userId);
-        }
+    public List<NotificationDto> getNotificationDtosByUserId(Long userId, boolean unreadOnly) {
+        List<Notification> notifications = notificationRepository.findByUser_UserIdOrderByCreatedAtDesc(userId);
+        return notifications.stream()
+                .map(notification -> NotificationDto.builder()
+                        .notificationId(notification.getNotificationId())
+                        .userId(notification.getUser().getUserId())
+                        .notificationType(notification.getNotificationType())
+                        .content(notification.getContent())
+                        .isRead(notification.isRead())
+                        .createdAt(notification.getCreatedAt())
+                        .relatedId(notification.getRelatedId())
+                        .build())
+                .collect(Collectors.toList());
     }
 
     @Override
